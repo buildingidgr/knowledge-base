272b4feb200dc5e0b526143c8fba5cab1dbfa559
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2025-02-14 21:52:37 +0200

    gfghg

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -6,27 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = {
-  async find(ctx) {
-    try {
-      const entries = await strapi.entityService.findMany('api::article.article', {
-        ...ctx.query,
-      });
-      return { data: entries };
-    } catch (error) {
-      ctx.throw(500, error);
-    }
-  },
-
-  async findOne(ctx) {
-    try {
-      const { id } = ctx.params;
-      const entry = await strapi.entityService.findOne('api::article.article', id, {
-        ...ctx.query,
-      });
-      return { data: entry };
-    } catch (error) {
-      ctx.throw(500, error);
-    }
-  },
-}; 
+module.exports = createCoreController('api::article.article'); 
--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -6,25 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = {
-  routes: [
-    {
-      method: 'GET',
-      path: '/api/articles',
-      handler: 'api::article.article.find',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'GET',
-      path: '/api/articles/:id',
-      handler: 'api::article.article.findOne',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-}; 
+module.exports = createCoreRouter('api::article.article'); 
