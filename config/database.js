54aa3c809a5e1ead7018f95cff8aafb5de3fb0dd
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2025-02-14 14:52:46 +0200

    sdf

--- config/database.js
+++ config/database.js
@@ -6,13 +6,13 @@ module.exports = ({ env }) => ({
           filename: env('DATABASE_FILENAME', '.tmp/data.db'),
         }
       : {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', ''),
+          password: env('DATABASE_PASSWORD', ''),
           ssl: env.bool('DATABASE_SSL', false) && {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+            rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
           },
         },
     useNullAsDefault: true,
