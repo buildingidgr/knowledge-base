d127a0cd5531abdc3042689b33affdf1f17ecae0
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2025-02-14 20:06:55 +0200

    asdf

--- config/api.ts
+++ config/api.ts
@@ -1,7 +1,13 @@
-export default {
+export default ({ env }) => ({
   rest: {
     defaultLimit: 25,
     maxLimit: 100,
     withCount: true,
   },
-};
+  responses: {
+    privateAttributes: [],
+  },
+  auth: {
+    scope: ['find', 'findOne', 'create', 'update', 'delete'],
+  },
+});
