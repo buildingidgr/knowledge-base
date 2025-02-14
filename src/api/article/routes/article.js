fcdbc945e42db4581a4dc0b899cf6fa1753cdb0e
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2025-02-14 19:58:50 +0200

    sdfg

--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -6,29 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::article.article', {
-  config: {
-    find: {
-      auth: false,
-      policies: [],
-      middlewares: [],
-    },
-    findOne: {
-      auth: false,
-      policies: [],
-      middlewares: [],
-    },
-    create: {
-      policies: [],
-      middlewares: [],
-    },
-    update: {
-      policies: [],
-      middlewares: [],
-    },
-    delete: {
-      policies: [],
-      middlewares: [],
-    },
-  },
-}); 
+module.exports = createCoreRouter('api::article.article'); 
