8adb4f1cf96a4f8889d45ccde12509372cc6c741
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2025-02-15 10:23:19 +0200

    dsfg

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,16 +1,29 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
   {
-    name: 'strapi::cors',
+    name: 'strapi::security',
     config: {
-      enabled: true,
-      headers: '*',
-      origin: ['*']
-    }
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+      cors: {
+        enabled: true,
+        origin: ['http://localhost:3000', 'http://localhost:1337', '*'], // Including localhost and allowing all origins for development
+        headers: ['*'],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+        keepHeaderOnError: true,
+      },
+    },
   },
-  'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
